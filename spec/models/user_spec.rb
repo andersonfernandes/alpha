88a9e3acf53179b998db1217a575349051396e53
Anderson Fernandes <fernandesanderson14@gmail.com>
2016-01-17 21:22:43 -0300

    Add Validations to the user
    - add user name to registration form
    
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,46 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  name                   :string           default(""), not null
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :inet
+#  last_sign_in_ip        :inet
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+
+  describe "validations" do
+    before do
+      user.valid?
+    end
+
+    it 'should be invalid with name null' do
+      user.name = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'should be invalid with email null' do
+      user.email = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'should be invalid with password null' do
+      user.password = nil
+      expect(user).not_to be_valid
+    end
+  end
 end
