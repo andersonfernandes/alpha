33a17a40d4d05313df478b9336310e463a55ecf0
Anderson Fernandes <fernandesanderson14@gmail.com>
2016-01-18 23:20:00 -0300

    Changed sign in and sign out page
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,5 +38,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates_presence_of :name, :email, :password
+  validates_presence_of :name, :email, :password, :password_confirmation
 end
